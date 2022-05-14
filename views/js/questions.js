06fb3ef0d4e9c3eb55ae3b63044fc4af4e3d0c73
YasinSharafieh <94445318+YasinSharafieh@users.noreply.github.com>
2022-05-14 19:34:04 +0430

    SamQuiz

--- views/js/questions.js
+++ views/js/questions.js
@@ -55,8 +55,6 @@ let questions = [
       "eXamine Multiple Language"
     ]
   },
-
-
   // you can uncomment the below codes and make duplicate as more as you want to add question
   // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
 
--- views/js/script.js
+++ views/js/script.js
@@ -150,12 +150,12 @@ function showResult(){
     quiz_box.classList.remove("activeQuiz"); //hide quiz box
     result_box.classList.add("activeResult"); //show result box
     const scoreText = result_box.querySelector(".score_text");
-    if (userScore > 3){ // if user scored more than 3
+    if (userScore > 6){ // if user scored more than 6
         //creating a new span tag and passing the user score number and total question number
         let scoreTag = '<span>and congrats! 🎉, You got <p>'+ userScore +'</p> out of <p>'+ questions.length +'</p></span>';
         scoreText.innerHTML = scoreTag;  //adding new span tag inside score_Text
     }
-    else if(userScore > 1){ // if user scored more than 1
+    else if(userScore > 3){ // if user scored more than 3
         let scoreTag = '<span>and nice 😎, You got <p>'+ userScore +'</p> out of <p>'+ questions.length +'</p></span>';
         scoreText.innerHTML = scoreTag;
     }
